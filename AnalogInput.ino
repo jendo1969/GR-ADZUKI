96802000f995b0381d7ed4b4d88b453e417e8984
jendo1969 <bzq00764@nifty.ne.jp>
2017-12-02 21:25:25 +0900

    Update AnalogInput.ino

--- AnalogInput.ino
+++ AnalogInput.ino
@@ -1,25 +1,25 @@
 /*
-  Analog Input 
-*/
+  Analog Input
+ */
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
+int sensorPin = A2;    // select the input pin for the light sensor
+int LED6 = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
   // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);  
+  pinMode(LED6, OUTPUT);  
 }
 
 void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);    
   // turn the ledPin on
-  digitalWrite(ledPin, HIGH);  
+  digitalWrite(LED6, HIGH);  
   // stop the program for <sensorValue> milliseconds:
   delay(sensorValue);          
   // turn the ledPin off:        
-  digitalWrite(ledPin, LOW);   
+  digitalWrite(LED6, LOW);   
   // stop the program for for <sensorValue> milliseconds:
   delay(sensorValue);                  
 }
